18afc2cf3cec59c1d83fbfff7694edaf2a7a1c81
hieuquoc <hieubk21292@gmail.com>
2026-03-06 15:38:29 +0000

    moves

--- Assets/Scripts/BlockMove.cs
+++ Assets/Scripts/BlockMove.cs
@@ -7,14 +7,16 @@ public class BlockMove : BaseMoveObstacle
     
     public float MoveSpeed = 5f;
     public float MoveDistance = 2f;
+    public float ChangeTime = 0f;
     [SerializeField] private Vector3 startPosition;
     
     [SerializeField] private Transform visualTransform;
-
-    [SerializeField] private Vector3 currentMoveVector;
     [SerializeField] private bool autoStart = false;
     [SerializeField] private Vector3 moveVector;
     [SerializeField] private float curMoveSpeed = 0f;
+    [SerializeField] private float currentChangeTime = 0f;
+    [SerializeField] private int direction = 1;
+
 
     void Awake()
     {
@@ -32,24 +34,43 @@ public class BlockMove : BaseMoveObstacle
     public override void SetUp()
     {
         base.SetUp();
-        currentMoveVector = moveVector;
         float distance = Random.Range(0, MoveDistance);
         visualTransform.localPosition = startPosition + moveVector.normalized * distance;       
         curMoveSpeed = Random.Range(MoveSpeed * 0.5f, MoveSpeed);
+        currentChangeTime = ChangeTime;
         Debug.Log($"Set up BlockMove at {visualTransform.localPosition} with move vector {moveVector} and distance {distance}"); 
+        direction = 1;
     }
     // Update is called once per frame
     void Update()
     {
-        if (Distance() >= MoveDistance)
+        if (Distance() >= MoveDistance && direction == 1)
         {
-            currentMoveVector = -moveVector;
+            if(currentChangeTime > 0)
+            {
+                currentChangeTime -= Time.deltaTime;
+                if(currentChangeTime <= 0)
+                {
+                    currentChangeTime = ChangeTime;
+                    direction = -1;
+                }
+                return;
+            }
         }
-        else if (Distance() <= 0.1f)
+        else if (Distance() <= 0.1f && direction == -1)
         {
-            currentMoveVector = moveVector;
+            if(currentChangeTime > 0)
+            {
+                currentChangeTime -= Time.deltaTime;
+                if(currentChangeTime <= 0)
+                {
+                    currentChangeTime = ChangeTime;
+                    direction = 1;
+                }
+                return;
+            }
         }
-        visualTransform.localPosition += currentMoveVector * curMoveSpeed * Time.deltaTime;
+        visualTransform.localPosition += moveVector * curMoveSpeed * direction * Time.deltaTime;
     }
 
     public float Distance()
